9cb4e36e3eaa8f500e44120736b1dc5903ca16f5
Tramac <1145955920@qq.com>
2018-10-23 02:23:23 -0500

    Create 2_1.cpp

---
+++ code/1-Chapter/2_1.cpp
@@ -0,0 +1,12 @@
+//显示图像
+#include "opencv/cv.h"
+#include "opencv/highgui.h"
+
+int main() {
+	IplImage *img = cvLoadImage("./Lena.jpg");
+	cvNamedWindow("Example1", CV_WINDOW_AUTOSIZE);
+	cvShowImage("Example1", img);
+	cvWaitKey(0);
+	cvReleaseImage(&img);
+	cvDestroyWindow("Example1");
+}
