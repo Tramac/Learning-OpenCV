4c8f9231001c8a68cc59d22ebf4605a885bcac5b
Tramac <1145955920@qq.com>
2018-10-23 02:31:39 -0500

    Update 2-1.cpp

--- code/2-Chapter/2-1.cpp
+++ code/2-Chapter/2-1.cpp
@@ -2,10 +2,10 @@
 #include "opencv/highgui.h"
 
 int main() {
-	IplImage *img = cvLoadImage("./Lena.jpg");
-	cvNamedWindow("Example1", CV_WINDOW_AUTOSIZE);
-	cvShowImage("Example1", img);
-	cvWaitKey(0);
-	cvReleaseImage(&img);
-	cvDestroyWindow("Example1");
+    IplImage *img = cvLoadImage("./Lena.jpg");
+    cvNamedWindow("Example1", CV_WINDOW_AUTOSIZE);
+    cvShowImage("Example1", img);
+    cvWaitKey(0);
+    cvReleaseImage(&img);
+    cvDestroyWindow("Example1");
 }
